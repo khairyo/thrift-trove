71fc32e8965b7cef873fb283684d10b461db3fa7
khairyo <lim.khai.ryo@gmail.com>
2024-08-27 00:11:22 +0800

    (not working) webhooks for stripe - delete item if payment successful

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,8 @@ const bodyParser = require('body-parser');
 const app = express();
 require('dotenv').config();
 
-
 app.use(bodyParser.json());
-app.use(express.json())
+app.use(express.json());
 app.use(cors({ credentials: true, origin: true }));
 
 app.get('/', (req, res) => {
@@ -14,25 +13,27 @@ app.get('/', (req, res) => {
 });
 
 // Account & User Profile & Address
-app.use("/api/account", require("./routes/account.route"))
-// app.use("/api/user-profile", require("./routes/userProfile.route"))
+app.use("/api/account", require("./routes/account.route"));
 
 // Product & Cart
-app.use("/api/product", require("./routes/product.route"))
-app.use("/api/cart", require("./routes/cart.route"))
+app.use("/api/product", require("./routes/product.route"));
+app.use("/api/cart", require("./routes/cart.route"));
 
 // My Purchases & My Sales
-// app.use("/api/mypurchases", require("./routes/mypurchases.route"))
-// app.use("/api/mysales", require("./routes/mysales.route"))
+// app.use("/api/mypurchases", require("./routes/mypurchases.route"));
+// app.use("/api/mysales", require("./routes/mysales.route"));
 
 // Payment (Stripe)
 app.use("/api/payments", require("./routes/payment.route"));
 
+// Webhook route for Stripe (important: use express.raw)
+app.post('/webhook', express.raw({ type: 'application/json' }), require('./controllers/payment.controller').webhook);
+
 // Blogs
-// app.use("/api/blog", require('./routes/blog.route'))
+// app.use("/api/blog", require('./routes/blog.route'));
 
 app.use(bodyParser.json());
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started on port ${process.env.PORT}`);
-})
+});
